df8a4b8b55f4103d539aaa9623d06987837ac6d2
Jose Q <jocequintana@gmail.com>
2023-11-19 15:13:19 -0300

    Recipe detail style

--- app/src/main/java/jose/recipesapp/presentation/detail/DetailFragment.kt
+++ app/src/main/java/jose/recipesapp/presentation/detail/DetailFragment.kt
@@ -1,14 +1,23 @@
 package jose.recipesapp.presentation.detail
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import jose.recipesapp.R
+import java.util.regex.Pattern
+
 
 class DetailFragment : Fragment() {
     private val args: DetailFragmentArgs by navArgs()
@@ -25,7 +34,35 @@ class DetailFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         val recipe = args.recipe
-        val titleTextView = view.findViewById<TextView>(R.id.recipesTitleTextView)
-        titleTextView.text = recipe.name
+        val recipeImageView = view.findViewById<ImageView>(R.id.recipeImageView)
+        val recipeNameTextView = view.findViewById<TextView>(R.id.recipeNameTextView)
+        val descriptionTextView = view.findViewById<TextView>(R.id.recipeDescriptionTextView)
+        val recipeCategoryTextView = view.findViewById<TextView>(R.id.recipeCategoryTextView)
+        val recipeAreaTextView = view.findViewById<TextView>(R.id.recipeAreaTextView)
+        val recipeYoutubeTextView = view.findViewById<TextView>(R.id.recipeYoutubeLinkTextView)
+        val recipeSourceTextView = view.findViewById<TextView>(R.id.recipeSourceLinkTextView)
+        val backArrowImageView = view.findViewById<ImageView>(R.id.backArrowImageView)
+        backArrowImageView.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        recipeNameTextView.text = recipe.name
+        descriptionTextView.text = recipe.description
+        recipeCategoryTextView.text = recipe.category
+        recipeAreaTextView.text = recipe.area
+        recipeYoutubeTextView.setOnClickListener {
+            val url = recipe.youtubeLink
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+        recipeSourceTextView.setOnClickListener {
+            val url = recipe.sourceLink
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+        Glide.with(recipeImageView)
+            .load(recipe.image)
+            .into(recipeImageView)
     }
 }
