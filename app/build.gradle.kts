7263ba6c3654dd904bb29576f45b884043241b41
Jose Q <jocequintana@gmail.com>
2023-11-20 13:06:45 -0300

    Updating libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.1")
 
     implementation("androidx.multidex:multidex:2.0.1")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     val navVersion = "2.7.5"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -70,7 +70,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.12.2")
-    testImplementation("org.mockito:mockito-core:1.10.19")
+    testImplementation("org.mockito:mockito-core:2.25.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
